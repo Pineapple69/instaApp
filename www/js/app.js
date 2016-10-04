cebde2907248419e9925b0a259b37dff696ac5f6
Софья Старикова <sofastarikova@MacBook-Pro.local>
2016-10-04 16:58:39 +0300

    Phase 2

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services',])
+var app = angular.module('app', ['ionic','ngCordova', 'app.controllers', 'app.routes', 'app.directives','app.services',])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,3 +21,81 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives
     }
   });
 })
+
+
+app.controller('AppCtrl', function($scope) {
+ $scope.data = {};
+ $scope.name = "";
+ 
+ $scope.saveName = function() {
+    $scope.name = "Myprofile666: " + $scope.data.firstName;
+  
+  };
+});
+
+app.controller('MainCtrl', function($scope, $cordovaCamera) {
+    $scope.takeImage = function() {
+        var options = {
+            quality: 80,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: Camera.PictureSourceType.CAMERA,
+            allowEdit: true,
+            encodingType: Camera.EncodingType.JPEG,
+            targetWidth: 250,
+            targetHeight: 250,
+            popoverOptions: CameraPopoverOptions,
+            saveToPhotoAlbum: false
+        };
+         
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+            $scope.srcImage = "data:image/jpeg;base64," + imageData;
+        }, function(err) {
+            // error
+        });
+    }
+});
+app.controller('ImagePickerController', function($scope, $cordovaImagePicker, $ionicPlatform, $cordovaContacts) {
+ 
+    $scope.collection = {
+        selectedImage : ''
+    };
+ 
+    $ionicPlatform.ready(function() {
+ 
+        $scope.getImageSaveContact = function() {       
+            // Image picker will load images according to these settings
+            var options = {
+                maximumImagesCount: 1, // Max number of selected images, I'm using only one for this example
+                width: 800,
+                height: 800,
+                quality: 80            // Higher is better
+            };
+ 
+            $cordovaImagePicker.getPictures(options).then(function (results) {
+                // Loop through acquired images
+                for (var i = 0; i < results.length; i++) {
+                    $scope.collection.selectedImage = results[i];   // We loading only one image so we can use it like this
+ 
+                    window.plugins.Base64.encodeFile($scope.collection.selectedImage, function(base64){  // Encode URI to Base64 needed for contacts plugin
+                        $scope.collection.selectedImage = base64;
+                        $scope.addContact();    // Save contact
+                    });
+                }
+            }, function(error) {
+                console.log('Error: ' + JSON.stringify(error));    // In case of error
+            });
+        };  
+ 
+    }); 
+
+ 
+
+});
+
+
+
+
+
+
+
+
--- www/js/services.js
+++ www/js/services.js
@@ -7,3 +7,25 @@ angular.module('app.services', [])
 .service('BlankService', [function(){
 
 }]);
+
+app.factory('Posts', function() {
+  // Post Data
+  var posts = [{
+    id: 0,
+    name: 'ooooo',
+    avatar: 'img/oooo.png',
+    image: 'img/oooo.png',
+    likes: 1,
+    caption: 'first caption'
+  }];
+
+  return {
+    all: function() {
+      return posts;
+    },
+    share: function(newdata) {
+      posts.push(newdata);
+      return posts;
+    }
+  };
+});
